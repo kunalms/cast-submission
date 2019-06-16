345532088b5e60323c8bcd91ba45a386829cee9d
Kunal Sharma <kunal.sharma@firstinsight.com>
2019-06-16 12:51:28 +0530

    initial setup

---
+++ src/main/java/com/example/casthiring/casthiring/CasthiringApplication.java
@@ -0,0 +1,13 @@
+package com.example.casthiring.casthiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasthiringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasthiringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/casthiring/casthiring/CasthiringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.casthiring.casthiring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CasthiringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
